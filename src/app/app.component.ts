f0e9225efd08e1003c5047bdc0f5efe3f5281695
VKozhura <kozhura_v@mail.ru>
2022-07-17 19:45:12 +0300

    1ch - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<label>Введите имя:</label>
+    <input [(ngModel)]="name" placeholder="name" />
+    <h1>Добро пожаловать, {{ name }}!</h1>`,
 })
 export class AppComponent {
-  title = 'angular-components';
+  name = '';
 }
