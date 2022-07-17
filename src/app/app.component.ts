f8873d6389e54db465be16b05367924048b93296
VKozhura <kozhura_v@mail.ru>
2022-07-18 00:30:42 +0300

    приявязка к событию

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  count: number = 0;
+
+  increase(): void {
+    this.count++;
+  }
+}
