9e528816298ca323edb1234920ca80cf0bc842ac
VKozhura <kozhura_v@mail.ru>
2022-07-17 20:30:16 +0300

    3ch - стили и шаблоны

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello Angular</h1>
-    <p>Angular 14 представляет модульную архитектуру приложения</p>`,
-  styles: [
-    `
-      h1,
-      h2 {
-        color: navy;
-      }
-
-      p {
-        font-size: 13px;
-        font-family: Verdana;
-      }
-    `,
-  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
