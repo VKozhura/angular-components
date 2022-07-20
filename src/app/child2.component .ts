826db78252c8c4fdf467fccade4ad043bb505f0a
VKozhura <kozhura_v@mail.ru>
2022-07-20 10:56:19 +0300

    привязка к сеттеру 2

--- src/app/child2.component .ts
+++ src/app/child2.component .ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'child2-comp',
   template: `
     <p>Имя пользователя: {{userName2}}</p>
-    <p>Возраст пользователя: {{userAge2}}</p>`,
+    <p>Возраст пользователя: {{userAge}}</p>`,
   styles: [
     `
       h2,
@@ -18,9 +18,9 @@ export class Child2Component {
   @Input() userName2: string = ''; //входное свойство
   _userAge2: number = 0;
 
+  // установка значения userAge2
   @Input()
-
-  set userAge2(age: number) {
+  set userAge(age: number) {
     if (age < 0) {
       this._userAge2 = 0
     } else if (age > 100) {
@@ -30,7 +30,7 @@ export class Child2Component {
     }
   }
 
-  get userAge2() {
+  get userAge() {
     return this._userAge2
   }
 }
