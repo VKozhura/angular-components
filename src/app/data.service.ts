829bc01a4ed0bb8614dee4c21000e5ade6f5ee65
VKozhura <kozhura_v@mail.ru>
2022-07-25 12:04:26 +0300

    Опциональные сервисы

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,13 +1,23 @@
+import { Injectable, Optional } from "@angular/core";
+import { LogService } from "./log.service";
+
+
+
+@Injectable() // к классу сервиса применется декоратор, чтобы указать, что сервис может использоваться в других сервисах
 export class DataService {
     
     private data: string[] = ["Apple iPhone XR", "Samsung Galaxy S9",  "Nokia 9"];
+
+    constructor(@Optional() private logService: LogService) {}
     
     getData(): string[] {
+        if (this.logService) this.logService.write('операция получения данных');
         return this.data
     }
 
     addData(name: string) {
         this.data.push(name);
+        if (this.logService) this.logService.write("операция добавления данных");
     }
 
 }
