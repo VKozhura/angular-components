64367f184c9bd0ec1420142daa1e99b7dc267ea9
VKozhura <kozhura_v@mail.ru>
2022-07-25 16:30:36 +0300

    Получение и изменение модели

--- src/app/form.component.ts
+++ src/app/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { NgModel } from "@angular/forms";
 
 export class Phone {
     constructor(
@@ -21,7 +22,19 @@ export class FormComponent {
     phones: Phone[] = [];
     companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
     
-    addPhone(){
-        this.phones.push(new Phone(this.phone.title, this.phone.price, this.phone.company));
+    // addPhone(){
+    //     this.phones.push(new Phone(this.phone.title, this.phone.price, this.phone.company));
+    // }
+
+    addPhone(title:NgModel, price: NgModel, comp: NgModel){
+        console.log(title);
+        console.log(price);
+        console.log(comp);
+    }
+
+    onTitleChange() {
+        if(this.phone.title=="нет") {
+            this.phone.title = "неизвестно";
+        }
     }
 }
